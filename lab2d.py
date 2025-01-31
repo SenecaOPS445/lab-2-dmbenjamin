ede66b77f4da1078c4289b8e88fc798dd864b142
Darian Benjamin <darianmikhail@gmail.com>
2025-01-31 00:53:29 -0500

    Added lab2d

--- lab2d.py
+++ lab2d.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+
 import sys
 
-
 if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <name> <age>")
-    sys.exit(0)
-
+    print("Usage: ./lab2d.py name age")
+    sys.exit(0)  # Exit with 0 instead of 1
 
 name = sys.argv[1]
 age = sys.argv[2]
 
 print(f"Hi {name}, you are {age} years old.")
+
 
 
